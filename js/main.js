5da35c8a19c030db3913566ae9a5a7ef6a0cac5c
shengyyds <1346930348@qq.com>
2023-09-16 12:38:52 +0800

    Site updated: 2023-09-16 12:38:51

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', function () {
       this.classList.toggle('expand-done')
     }
 
-    function createEle (lang, item, service) {
+    function createEle(lang, item, service) {
       const fragment = document.createDocumentFragment()
 
       if (isShowTool) {
@@ -193,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function () {
   /**
    * PhotoFigcaption
    */
-  function addPhotoFigcaption () {
+  function addPhotoFigcaption() {
     document.querySelectorAll('#article-container img').forEach(function (item) {
       const parentEle = item.parentNode
       const altValue = item.title || item.alt
